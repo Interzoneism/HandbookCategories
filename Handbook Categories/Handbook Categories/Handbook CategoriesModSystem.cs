d1d7ce629b45adf142b196d5db35502c817c7730
Interzoneism <martinforslund.studio@gmail.com>
2025-09-24 22:55:39 +0200

    Revert "Implement CBR spawn protection and overlay"
    This reverts commit 104ba0c16018ee46a2c994f6059c2a961b0825df.

--- Handbook Categories/Handbook Categories/Handbook CategoriesModSystem.cs
+++ Handbook Categories/Handbook Categories/Handbook CategoriesModSystem.cs
@@ -1,67 +1,13 @@
-using System;
-using Vintagestory.API.Client;
+﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace Handbook_Categories;
-
-public class Handbook_CategoriesModSystem : ModSystem
+namespace Handbook_Categories
 {
-    private CbrConfig _config;
-    private CbrBlockMetadataProvider _metadataProvider;
-    private SpawnProtectionManager _serverManager;
-    private CbrOverlayManager _overlayManager;
-
-    public override void Start(ICoreAPI api)
+    public class Handbook_CategoriesModSystem : ModSystem
     {
-        base.Start(api);
 
-        _config = api.LoadModConfig<CbrConfig>(CbrConfig.ConfigFileName) ?? new CbrConfig();
-        _config.EnsureValidity();
-        api.StoreModConfig(_config, CbrConfig.ConfigFileName);
 
-        _metadataProvider = new CbrBlockMetadataProvider(api, _config);
-    }
-
-    public override void AssetsFinalize(ICoreAPI api)
-    {
-        base.AssetsFinalize(api);
-        _metadataProvider?.ClearCache();
-    }
-
-    public override void StartServerSide(ICoreServerAPI api)
-    {
-        base.StartServerSide(api);
-        EnsureMetadataProvider(api);
-        _serverManager = new SpawnProtectionManager(api, _config, _metadataProvider);
-    }
-
-    public override void StartClientSide(ICoreClientAPI api)
-    {
-        base.StartClientSide(api);
-        EnsureMetadataProvider(api);
-        _overlayManager = new CbrOverlayManager(api, _config, _metadataProvider);
-    }
-
-    public override void Dispose()
-    {
-        base.Dispose();
-        _overlayManager?.Dispose();
-        _overlayManager = null;
-        _serverManager?.Dispose();
-        _serverManager = null;
-        _metadataProvider = null;
-    }
-
-    private void EnsureMetadataProvider(ICoreAPI api)
-    {
-        if (_metadataProvider == null)
-        {
-            _metadataProvider = new CbrBlockMetadataProvider(api, _config);
-        }
-        else
-        {
-            _metadataProvider.UpdateApi(api);
-        }
     }
 }
--- Handbook Categories/Handbook Categories/UnderfootType.cs
+++
@@ -1,8 +0,0 @@
-namespace Handbook_Categories;
-
-public enum UnderfootType
-{
-    None = 0,
-    Cheap = 1,
-    Premium = 2
-}
