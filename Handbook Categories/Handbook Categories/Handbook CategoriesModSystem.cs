9116c1cf12ca35d7987b4ec99bdbe8542d799296
Interzoneism <martinforslund.studio@gmail.com>
2025-09-24 19:28:48 +0200

    Add project files.

---
+++ Handbook Categories/Handbook Categories/Handbook CategoriesModSystem.cs
@@ -0,0 +1,13 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace Handbook_Categories
+{
+    public class Handbook_CategoriesModSystem : ModSystem
+    {
+
+
+    }
+}
