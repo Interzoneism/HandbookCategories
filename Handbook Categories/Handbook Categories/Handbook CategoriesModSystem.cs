625e6252a75fd2cc8e77ccd148f26e3ba7c5aabc
Interzoneism <martinforslund.studio@gmail.com>
2025-09-25 16:29:47 +0200

    padding

--- Handbook Categories/Handbook Categories/Handbook CategoriesModSystem.cs
+++ Handbook Categories/Handbook Categories/Handbook CategoriesModSystem.cs
@@ -128,7 +128,7 @@ namespace Handbook_Categories
                     DataInt = tabs.Count,
                     CategoryCode = categoryCode,
                     Name = HandbookCategoryManager.GetTabDisplayName(categoryCode),
-                    PaddingTop = tabs.Count == 0 ? 20.0 : 5.0
+                    PaddingTop = tabs.Count == 0 ? 10.0 : 2.0
                 };
 
                 tabs.Add(tab);
